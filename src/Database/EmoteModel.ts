93b7841f054e711787d388bb4f159c758043aa5e
Stultiens, Tobin <PS160507@summacollege.nl>
2019-10-14 11:10:54 +0200

    did not fix the database related issues, but i made some improvements to the database structure

--- src/Database/EmoteModel.ts
+++ src/Database/EmoteModel.ts
@@ -1,14 +1,16 @@
 import * as mongoose from "mongoose";
 
 export interface IEmote extends mongoose.Document {
-    shortcut: string;
-    id: number;
-  }
+  shortcut: string;
+  id: number;
+  fullCommand: string;
+}
 
 export const EmoteSchema = new mongoose.Schema({
-    id: { type: Number, required: true },
-    shortcut: { type: String, required: true },
-  });
+  id: { type: Number, required: true },
+  shortcut: { type: String, required: true },
+  fullCommand: { type: String, required: true }
+});
 
 const Emote = mongoose.model<IEmote>("Emote", EmoteSchema);
 export default Emote;
--- src/TSDiscordBot.ts
+++ src/TSDiscordBot.ts
@@ -1,8 +1,8 @@
 import Discord from "discord.js";
-import { StoreEmote } from "./Commands/StoreEmote";
+import { EmoteCommands } from "./Commands/EmoteCommands";
 
 const client = new Discord.Client();
-const storeEmote = new StoreEmote();
+const storeEmote = new EmoteCommands();
 
 export class TSDiscordBot {
     public start(token: string): void {
@@ -12,12 +12,19 @@ export class TSDiscordBot {
         });
 
         client.on("message", (msg: { content: string; reply: (arg0: string) => void; }) => {
-            if (msg.content.startsWith("!store")) {
-                storeEmote.Store(msg);
-            }
-            else if (msg.content.startsWith("!retrieve")){
-                storeEmote.Retrieve(msg);
-            }
+           // if(msg.content.match("(!retrieve.*|^!store \\<.*).!*")){
+                if (msg.content.startsWith("!store")) {
+                    storeEmote.Store(msg);
+                }
+                else if (msg.content.startsWith("!retrieve")){
+                    var test = storeEmote.Retrieve(msg);
+                    var str = new String(test);
+                    console.log(str);
+                   var emoji = client.emojis.find(emoj => emoj.name === str);
+                   console.log(emoji);
+                   msg.reply(`${emoji}`);
+                }
+           // }            
         });
 
         client.login(token);
